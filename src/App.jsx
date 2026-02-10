313ba08fc4fa253ac83e904c4e9076430f6210aa
VladSmus <vladsmusev2004@yandex.ru>
2026-02-10 15:16:52 +0300

    html made

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./App.css";
-
+import { Link, Route, Routes } from "react-router-dom ";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </div>
+  );
 }
-
-  
 
 // COMPONENT = Function that returns JSX
 export default App;
